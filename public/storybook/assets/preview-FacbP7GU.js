dc22225580e33be9c4d5ffaeadced7b2ce9aebb7
lcdias <luis.maisnet@gmail.com>
2024-01-03 04:55:43 -0300

    copy stoeybook for public

---
+++ public/storybook/assets/preview-FacbP7GU.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-Ip_T0voz.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-ihPjd-T5.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-ihPjd-T5.js","./iframe-Ip_T0voz.js","./index-65Hdt-Ci.js","./_commonjsHelpers-5-cIlDoe.js","./index-ogXoivrg.js","./index-OpicemMp.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
